e52140551b61e86bfd15518769dd00210d708fd9
Alik <Nigay_AO@bk.ru>
2021-10-26 07:34:03 +0300

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Alik Nigay on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
